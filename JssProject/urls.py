1c48bdf2521c3a96ff7104c2f9d499566ac352c7
sin jae joon <grandfinal12@likelion.org>
2020-08-07 16:51:13 +0900

    made Jss(until 3steps/9steps) but happende creat page error

---
+++ JssProject/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from main.views import index,create
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',index, name="index"),
+    path('create/', create, name="create"),
+]
